5c9e0ea1155181c6be6d4ae66342dcf6c925edee
jscss1122 <97178136+jscss1122@users.noreply.github.com>
2022-01-26 17:49:54 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,13 @@
+var a=20;
+if(a>10)
+{
+console.log("number is greater than 15");
+}
+else if (a==20)
+{
+console.log("number is equal to 15");
+}
+else
+{
+console.log("number is less than 15");
+}
